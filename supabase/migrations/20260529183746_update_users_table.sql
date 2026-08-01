33f41ab417cc2baaaf388d72a66f6887b7041189
Razen04 <73794553+Razen04@users.noreply.github.com>
2026-08-01 20:09:37 +0530

    Added about section is users table

--- supabase/migrations/20260529183746_update_users_table.sql
+++ supabase/migrations/20260529183746_update_users_table.sql
@@ -3,5 +3,10 @@
 alter table public.users 
 add column username text unique;
 
+alter table public.users 
+add column about text,
+add constraint users_about_max_length
+check (char_length(about) <= 100);
+
 alter table public.users
 add column is_public boolean default true;
